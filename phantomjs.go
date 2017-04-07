466a48c724a28a61cf5493034a93a93464c754dd
Ben Johnson <benbjohnson@yahoo.com>
2017-04-07 16:05:37 -0600

    Add CanGoBack, CanGoForward, ClipRect.

--- phantomjs.go
+++ phantomjs.go
@@ -2,7 +2,6 @@ package phantomjs
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -46,40 +45,6 @@ func NewProcess() *Process {
 	}
 }
 
-// Open start the phantomjs process with the shim script.
-func (p *Process) Open(ctx context.Context) error {
-	// Write shim to a temporary file.
-	f, err := ioutil.TempFile("", "phantomjs-")
-	if err != nil {
-		return err
-	} else if _, err := f.WriteString(shim); err != nil {
-		f.Close()
-		os.Remove(f.Name())
-		return err
-	} else if err := f.Close(); err != nil {
-		os.Remove(f.Name())
-		return err
-	}
-	p.path = f.Name()
-
-	// Start external process.
-	cmd := exec.Command(p.BinPath, p.path)
-	cmd.Env = []string{fmt.Sprintf("PORT=%d", p.Port)}
-	cmd.Stdout = p.Stdout
-	cmd.Stderr = p.Stderr
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	p.cmd = cmd
-
-	// Wait until process is available.
-	if err := p.wait(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Close stops the process.
 func (p *Process) Close() (err error) {
 	// Kill process.
@@ -87,9 +52,7 @@ func (p *Process) Close() (err error) {
 		if e := p.cmd.Process.Kill(); e != nil && err == nil {
 			err = e
 		}
-		if e := p.cmd.Wait(); e != nil && err == nil {
-			err = e
-		}
+		p.cmd.Wait()
 	}
 
 	// Remove shim file.
@@ -108,16 +71,19 @@ func (p *Process) URL() string {
 }
 
 // wait continually checks the process until it gets a response or times out.
-func (p *Process) wait(ctx context.Context) error {
+func (p *Process) wait() error {
 	ticker := time.NewTicker(1000 * time.Millisecond)
+	defer ticker.Stop()
+
+	timer := time.NewTimer(30 * time.Second)
 	defer ticker.Stop()
 
 	for {
 		select {
-		case <-ctx.Done():
+		case <-timer.C:
 			return errors.New("timeout")
 		case <-ticker.C:
-			if err := p.ping(ctx); err == nil {
+			if err := p.ping(); err == nil {
 				return nil
 			}
 		}
@@ -125,7 +91,7 @@ func (p *Process) wait(ctx context.Context) error {
 }
 
 // ping checks the process to see if it is up.
-func (p *Process) ping(ctx context.Context) error {
+func (p *Process) ping() error {
 	// Send request.
 	resp, err := http.Get(p.URL() + "/ping")
 	if err != nil {
@@ -141,7 +107,7 @@ func (p *Process) ping(ctx context.Context) error {
 }
 
 // CreateWebPage returns a new instance of a "webpage".
-func (p *Process) CreateWebPage(ctx context.Context) *WebPage {
+func (p *Process) CreateWebPage() *WebPage {
 	var resp struct {
 		Ref refJSON `json:"ref"`
 	}
@@ -213,18 +179,328 @@ func (p *WebPage) Open(url string) error {
 	return nil
 }
 
-// Close releases the web page and its resources.
-func (p *WebPage) Close() {
-	p.ref.process.mustDoJSON("POST", "/webpage/close", map[string]interface{}{"ref": p.ref.id}, nil)
+// CanGoBack returns true if the page can be navigated back.
+func (p *WebPage) CanGoBack() bool {
+	var resp struct {
+		Value bool `json:"value"`
+	}
+	p.ref.process.mustDoJSON("POST", "/webpage/can_go_back", map[string]interface{}{"ref": p.ref.id}, &resp)
+	return resp.Value
+}
+
+// CanGoForward returns true if the page can be navigated forward.
+func (p *WebPage) CanGoForward() bool {
+	var resp struct {
+		Value bool `json:"value"`
+	}
+	p.ref.process.mustDoJSON("POST", "/webpage/can_go_forward", map[string]interface{}{"ref": p.ref.id}, &resp)
+	return resp.Value
+}
+
+// ClipRect returns the clipping rectangle used when rendering.
+// Returns nil if no clipping rectangle is set.
+func (p *WebPage) ClipRect() Rect {
+	var resp struct {
+		Value rectJSON `json:"value"`
+	}
+	p.ref.process.mustDoJSON("POST", "/webpage/clip_rect", map[string]interface{}{"ref": p.ref.id}, &resp)
+	return Rect{
+		Top:    resp.Value.Top,
+		Left:   resp.Value.Left,
+		Width:  resp.Value.Width,
+		Height: resp.Value.Height,
+	}
+}
+
+// SetClipRect sets the clipping rectangle used when rendering.
+// Set to nil to render the entire webpage.
+func (p *WebPage) SetClipRect(rect Rect) {
+	req := map[string]interface{}{
+		"ref": p.ref.id,
+		"rect": rectJSON{
+			Top:    rect.Top,
+			Left:   rect.Left,
+			Width:  rect.Width,
+			Height: rect.Height,
+		},
+	}
+	p.ref.process.mustDoJSON("POST", "/webpage/set_clip_rect", req, nil)
 }
 
 // Content returns content of the webpage enclosed in an HTML/XML element.
 func (p *WebPage) Content() string {
 	var resp struct {
-		Content string `json:"content"`
+		Value string `json:"value"`
 	}
 	p.ref.process.mustDoJSON("POST", "/webpage/content", map[string]interface{}{"ref": p.ref.id}, &resp)
-	return resp.Content
+	return resp.Value
+}
+
+func (p *WebPage) Cookies() string {
+	panic("TODO")
+}
+
+func (p *WebPage) CustomHeaders() string {
+	panic("TODO")
+}
+
+func (p *WebPage) Event() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FocusedFrameName() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FrameContent() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FrameName() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FramePlainText() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FrameTitle() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FrameUrl() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FramesCount() string {
+	panic("TODO")
+}
+
+func (p *WebPage) FramesName() string {
+	panic("TODO")
+}
+
+func (p *WebPage) LibraryPath() string {
+	panic("TODO")
+}
+
+func (p *WebPage) NavigationLocked() string {
+	panic("TODO")
+}
+
+func (p *WebPage) OfflineStoragePath() string {
+	panic("TODO")
+}
+
+func (p *WebPage) OfflineStorageQuota() string {
+	panic("TODO")
+}
+
+func (p *WebPage) OwnsPages() string {
+	panic("TODO")
+}
+
+func (p *WebPage) PagesWindowName() string {
+	panic("TODO")
+}
+
+func (p *WebPage) Pages() string {
+	panic("TODO")
+}
+
+func (p *WebPage) PaperSize() string {
+	panic("TODO")
+}
+
+func (p *WebPage) PlainText() string {
+	panic("TODO")
+}
+
+func (p *WebPage) ScrollPosition() string {
+	panic("TODO")
+}
+
+func (p *WebPage) Settings() string {
+	panic("TODO")
+}
+
+func (p *WebPage) Title() string {
+	panic("TODO")
+}
+
+func (p *WebPage) Url() string {
+	panic("TODO")
+}
+
+func (p *WebPage) ViewportSize() string {
+	panic("TODO")
+}
+
+func (p *WebPage) WindowName() string {
+	panic("TODO")
+}
+
+func (p *WebPage) ZoomFactor() string {
+	panic("TODO")
+}
+
+func (p *WebPage) AddCookie() {
+	panic("TODO")
+}
+
+func (p *WebPage) ChildFramesCount() {
+	panic("TODO")
+}
+
+func (p *WebPage) ChildFramesName() {
+	panic("TODO")
+}
+
+func (p *WebPage) ClearCookies() {
+	panic("TODO")
+}
+
+// Close releases the web page and its resources.
+func (p *WebPage) Close() {
+	p.ref.process.mustDoJSON("POST", "/webpage/close", map[string]interface{}{"ref": p.ref.id}, nil)
+}
+
+func (p *WebPage) CurrentFrameName() {
+	panic("TODO")
+}
+
+func (p *WebPage) DeleteCookie() {
+	panic("TODO")
+}
+
+func (p *WebPage) EvaluateAsync() {
+	panic("TODO")
+}
+
+func (p *WebPage) EvaluateJavaScript() {
+	panic("TODO")
+}
+
+func (p *WebPage) Evaluate() {
+	panic("TODO")
+}
+
+func (p *WebPage) GetPage() {
+	panic("TODO")
+}
+
+func (p *WebPage) GoBack() {
+	panic("TODO")
+}
+
+func (p *WebPage) GoForward() {
+	panic("TODO")
+}
+
+func (p *WebPage) Go() {
+	panic("TODO")
+}
+
+func (p *WebPage) IncludeJs() {
+	panic("TODO")
+}
+
+func (p *WebPage) InjectJs() {
+	panic("TODO")
+}
+
+func (p *WebPage) OpenUrl() {
+	panic("TODO")
+}
+
+// Open start the phantomjs process with the shim script.
+func (p *Process) Open() error {
+	// Write shim to a temporary file.
+	f, err := ioutil.TempFile("", "phantomjs-")
+	if err != nil {
+		return err
+	} else if _, err := f.WriteString(shim); err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return err
+	} else if err := f.Close(); err != nil {
+		os.Remove(f.Name())
+		return err
+	}
+	p.path = f.Name()
+
+	// Start external process.
+	cmd := exec.Command(p.BinPath, p.path)
+	cmd.Env = []string{fmt.Sprintf("PORT=%d", p.Port)}
+	cmd.Stdout = p.Stdout
+	cmd.Stderr = p.Stderr
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	p.cmd = cmd
+
+	// Wait until process is available.
+	if err := p.wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *WebPage) Release() {
+	panic("TODO")
+}
+
+func (p *WebPage) Reload() {
+	panic("TODO")
+}
+
+func (p *WebPage) RenderBase64() {
+	panic("TODO")
+}
+
+func (p *WebPage) RenderBuffer() {
+	panic("TODO")
+}
+
+func (p *WebPage) Render() {
+	panic("TODO")
+}
+
+func (p *WebPage) SendEvent() {
+	panic("TODO")
+}
+
+func (p *WebPage) SetContent() {
+	panic("TODO")
+}
+
+func (p *WebPage) Stop() {
+	panic("TODO")
+}
+
+func (p *WebPage) SwitchToChildFrame() {
+	panic("TODO")
+}
+
+func (p *WebPage) SwitchToFocusedFrame() {
+	panic("TODO")
+}
+
+func (p *WebPage) SwitchToFrame() {
+	panic("TODO")
+}
+
+func (p *WebPage) SwitchToMainFrame() {
+	panic("TODO")
+}
+
+func (p *WebPage) SwitchToParentFrame() {
+	panic("TODO")
+}
+
+func (p *WebPage) UploadFile() {
+	panic("TODO")
 }
 
 // OpenWebPageSettings represents the settings object passed to WebPage.Open().
@@ -253,6 +529,22 @@ type refJSON struct {
 	ID string `json:"id"`
 }
 
+// Rect represents a rectangle used by WebPage.ClipRect().
+type Rect struct {
+	Top    int
+	Left   int
+	Width  int
+	Height int
+}
+
+// rectJSON is a struct for encoding rects as JSON.
+type rectJSON struct {
+	Top    int `json:"top"`
+	Left   int `json:"left"`
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
 // shim is the included javascript used to communicate with PhantomJS.
 const shim = `
 var system = require("system")
@@ -269,9 +561,13 @@ server.listen(system.env["PORT"], function(request, response) {
 	try {
 		switch (request.url) {
 			case '/ping': return handlePing(request, response);
+			case '/webpage/can_go_back': return handleWebpageCanGoBack(request, response);
+			case '/webpage/can_go_forward': return handleWebpageCanGoForward(request, response);
+			case '/webpage/clip_rect': return handleWebpageClipRect(request, response);
+			case '/webpage/set_clip_rect': return handleWebpageSetClipRect(request, response);
 			case '/webpage/create': return handleWebpageCreate(request, response);
-			case '/webpage/open': return handleWebpageOpen(request, response);
 			case '/webpage/content': return handleWebpageContent(request, response);
+			case '/webpage/open': return handleWebpageOpen(request, response);
 			case '/webpage/close': return handleWebpageClose(request, response);
 			default: return handleNotFound(request, response);
 		}
@@ -285,6 +581,31 @@ server.listen(system.env["PORT"], function(request, response) {
 function handlePing(request, response) {
 	response.statusCode = 200;
 	response.write('ok');
+	response.closeGracefully();
+}
+
+function handleWebpageCanGoBack(request, response) {
+	var page = ref(JSON.parse(request.post).ref);
+	response.write(JSON.stringify({value: page.canGoBack}));
+	response.closeGracefully();
+}
+
+function handleWebpageCanGoForward(request, response) {
+	var page = ref(JSON.parse(request.post).ref);
+	response.write(JSON.stringify({value: page.canGoForward}));
+	response.closeGracefully();
+}
+
+function handleWebpageClipRect(request, response) {
+	var page = ref(JSON.parse(request.post).ref);
+	response.write(JSON.stringify({value: page.clipRect}));
+	response.closeGracefully();
+}
+
+function handleWebpageSetClipRect(request, response) {
+	var msg = JSON.parse(request.post);
+	var page = ref(msg.ref);
+	page.clipRect = msg.rect;
 	response.closeGracefully();
 }
 
@@ -306,7 +627,7 @@ function handleWebpageOpen(request, response) {
 
 function handleWebpageContent(request, response) {
 	var page = ref(JSON.parse(request.post).ref)
-	response.write(JSON.stringify({content: page.content}));
+	response.write(JSON.stringify({value: page.content}));
 	response.closeGracefully();
 }
 
@@ -314,6 +635,7 @@ function handleWebpageClose(request, response) {
 	var msg = JSON.parse(request.post)
 	var page = ref(msg.ref)
 	page.close()
+	delete(refs, msg.ref)
 	response.statusCode = 200;
 	response.closeGracefully();
 }
--- phantomjs_test.go
+++ phantomjs_test.go
@@ -1,18 +1,61 @@
 package phantomjs_test
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/middlemost/phantomjs"
 )
 
-// Ensure process can create a webpage.
-func TestProcess_WebPage(t *testing.T) {
-	ctx := context.Background()
+// Ensure web page can return whether it can navigate forward.
+func TestWebPage_CanGoForward(t *testing.T) {
+	p := MustOpenNewProcess()
+	defer p.MustClose()
 
+	page := p.CreateWebPage()
+	defer page.Close()
+	if page.CanGoForward() {
+		t.Fatal("expected false")
+	}
+}
+
+// Ensure process can return if the page can navigate back.
+func TestWebPage_CanGoBack(t *testing.T) {
+	p := MustOpenNewProcess()
+	defer p.MustClose()
+
+	page := p.CreateWebPage()
+	defer page.Close()
+	if page.CanGoBack() {
+		t.Fatal("expected false")
+	}
+}
+
+// Ensure process can set and retrieve the clipping rectangle.
+func TestWebPage_ClipRect(t *testing.T) {
+	p := MustOpenNewProcess()
+	defer p.MustClose()
+
+	page := p.CreateWebPage()
+	defer page.Close()
+
+	// Clipping rectangle should be empty initially.
+	if v := page.ClipRect(); v != (phantomjs.Rect{}) {
+		t.Fatalf("expected empty rect: %#v", v)
+	}
+
+	// Set a rectangle.
+	rect := phantomjs.Rect{Top: 1, Left: 2, Width: 3, Height: 4}
+	page.SetClipRect(rect)
+	if v := page.ClipRect(); !reflect.DeepEqual(v, rect) {
+		t.Fatalf("unexpected value: %#v", v)
+	}
+}
+
+// Ensure web page can return its contents.
+func TestWebPage_Content(t *testing.T) {
 	// Serve web page.
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<html><body>OK</body></html>"))
@@ -20,20 +63,41 @@ func TestProcess_WebPage(t *testing.T) {
 	defer srv.Close()
 
 	// Start process.
-	p := phantomjs.NewProcess()
-	if err := p.Open(ctx); err != nil {
-		t.Fatal(err)
-	}
-	defer p.Close()
+	p := MustOpenNewProcess()
+	defer p.MustClose()
 
-	// Create page.
-	page := p.CreateWebPage(ctx)
+	// Create & open page.
+	page := p.CreateWebPage()
 	defer page.Close()
-
-	// Open a web page.
 	if err := page.Open(srv.URL); err != nil {
 		t.Fatal(err)
 	} else if content := page.Content(); content != `<html><head></head><body>OK</body></html>` {
 		t.Fatalf("unexpected content: %q", content)
+	}
+}
+
+// Process is a test wrapper for phantomjs.Process.
+type Process struct {
+	*phantomjs.Process
+}
+
+// NewProcess returns a new, open Process.
+func NewProcess() *Process {
+	return &Process{Process: phantomjs.NewProcess()}
+}
+
+// MustOpenNewProcess returns a new, open Process. Panic on error.
+func MustOpenNewProcess() *Process {
+	p := NewProcess()
+	if err := p.Open(); err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// MustClose closes the process. Panic on error.
+func (p *Process) MustClose() {
+	if err := p.Close(); err != nil {
+		panic(err)
 	}
 }
